1583aa7d5c93d11c461de21b94a0920da741f4d8
Yanick Santos <yanickeduardo04@outlook.com>
2022-03-22 23:38:07 -0300

    primeiras aulas de js

---
+++ Vincular-Js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Aula 3 - Vinculando um documento Js
+ * @author: Yanick Santos
+ * 
+ */
+
+document.write("<h1>"+"Yanick Santos"+"</h1>")
---
+++ funcao/script.js
@@ -0,0 +1,9 @@
+/**
+ * JS - Aula 4 - Função simples
+ * @author: Yanick Santos
+ */
+
+function Hello(){
+    var nome = prompt("Qual seu nome?")
+    alert("Hello!! "+ nome)
+}
