5fae5ba0d753da5ed9b62df1c1d10c1b375c2b64
Yanick Santos <yanickeduardo04@outlook.com>
2022-03-23 11:43:41 -0300

    adicionado mini projeto do jogo da lampada - incompleto

---
+++ Lampada/script.js
@@ -0,0 +1,12 @@
+/**
+ * JS - Aula: 5 - Calculadora Flex
+ * @author: Yanick Santos
+ */
+
+function Ligar(){
+    document.getElementById("lampada").src="img/on.jpg"
+}
+
+function Desligar(){
+    document.getElementById("lampada").src="img/off.jpg"
+}
